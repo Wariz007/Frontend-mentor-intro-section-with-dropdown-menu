0287ef6ab083212d8bedff7b73d9fc1bad5c1380
Abdulwariz <bookbodmas31@gmail.com>
2025-05-23 22:51:37 +0100

    updated dist and html for burger btn activation

--- dist/script.js
+++ dist/script.js
@@ -1,1 +1,11 @@
 "use strict";
+//MOBILE
+const burgerBtn = document.getElementById('burger');
+const burgerSlide = document.querySelector('.burger-slide');
+const burgerSlideCloseBtn = document.querySelector('.close-btn');
+burgerBtn === null || burgerBtn === void 0 ? void 0 : burgerBtn.addEventListener('click', () => {
+    burgerSlide.style.display = 'flex';
+});
+burgerSlideCloseBtn.addEventListener('click', () => {
+    burgerSlide.style.display = 'none';
+});
