e32e6d11b0641a699705aa0ba3c81b0aabbba487
Abdulwariz <bookbodmas31@gmail.com>
2025-05-25 11:21:54 +0100

    added code for burger buttons expansion

--- typescript/src/script.ts
+++ typescript/src/script.ts
@@ -2,11 +2,24 @@
 const burgerBtn = document.getElementById('burger');
 const burgerSlide = document.querySelector('.burger-slide') as HTMLElement;
 const burgerSlideCloseBtn = document.querySelector('.close-btn') as HTMLElement;
+const navAndContentsContainer = document.querySelector('.navigation-and-contents-container');
+const featuresBtn = document.getElementById('features-btn');
+const featuresDropDown = document.querySelector('.features-drop-down div') as HTMLElement;
+const companyBtn = document.querySelector('.company-dropdown');
+const companyDropdown = document.querySelector('.company-dropdown div') as HTMLElement;
 
 burgerBtn?.addEventListener('click', () => {
     burgerSlide.style.display = 'flex';
+    navAndContentsContainer?.classList.add('shadow');
 })
 burgerSlideCloseBtn.addEventListener('click', () => {
     burgerSlide.style.display = 'none';
+    navAndContentsContainer?.classList.remove('shadow');
 })
 
+featuresBtn?.addEventListener('click', () => {
+    featuresDropDown.classList.toggle('show');
+})
+companyBtn?.addEventListener('click', () => {
+    companyDropdown.classList.toggle('show');
+})
